ef8762e28ea61276a8e2c99ca82be10b36f49433
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-03-21 19:16:19 -0500

    hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,15 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
   ],
 };
